2e8fb592a7126c1317bc8e1ea4a4b8c32b184751
Sachin <vsachin220@gmail.com>
2022-09-29 13:56:33 +0530

    Project Init

---
+++ stack/models.py
@@ -0,0 +1,19 @@
+from pyexpat import model
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Questions(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.CharField(max_length=250)
+
+    def __str__(self) -> str:
+        return f"{self.user} asks {self.question}"
+
+class Answers(models.Model):
+    post = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=255)
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.posted_by} answers {self.post.question} by saying {self.answer}"
